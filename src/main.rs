3df856ed8b6045d7c4970dfe59d475fc27be885a
bobby3003 <roberto3003@riseup.net>
2021-09-20 03:33:57 +0700

    modified

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(5);
+    another_function(2, 3);
 }
-fn another_function(x: i32) {
+fn another_function(x: i8, y: i8) {
     println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
